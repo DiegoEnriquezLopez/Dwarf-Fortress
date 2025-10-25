d9942d70a27d8cf67568d59c4213de805adf3a9d
DiegoEnriquezLopez <diego.enriquezl2018@gmail.com>
2025-10-24 23:36:31 -0600

    Refactor Deque implementation using DqNode class

--- deque.py
+++ deque.py
@@ -1,84 +1,137 @@
-from doubly_linked_list import DoublyLinkedList
+class DqNode:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
 
 class Deque:
     def __init__(self, datos=None):
-        self._dll = DoublyLinkedList()
-        self._shadow = {}
-        self._left = 0
-        self._right = -1
+        self._head = None
+        self._tail = None
+        self._size = 0
         if datos is not None:
             try:
-                for d in datos:
-                    self.push_back(d)
+                for x in datos:
+                    self.push_back(x)
             except TypeError:
                 self.push_back(datos)
 
     def push_front(self, x):
-        self._dll.preppend(x)
-        self._left -= 1
-        self._shadow[self._left] = x
+        n = DqNode(x)
+        if self._head is None:
+            self._head = self._tail = n
+        else:
+            n.next = self._head
+            self._head.prev = n
+            self._head = n
+        self._size += 1
 
     def push_back(self, x):
-        self._dll.append(x)
-        self._right += 1
-        self._shadow[self._right] = x
+        n = DqNode(x)
+        if self._tail is None:
+            self._head = self._tail = n
+        else:
+            n.prev = self._tail
+            self._tail.next = n
+            self._tail = n
+        self._size += 1
 
     def pop_front(self):
-        if self.empty():
+        if self._head is None:
             return None
-        val = self._shadow.pop(self._left, None)
-        self._dll.remove(index=0)
-        self._left += 1
+        n = self._head
+        val = n.data
+        self._head = n.next
+        if self._head is None:
+            self._tail = None
+        else:
+            self._head.prev = None
+        self._size -= 1
         return val
 
     def pop_back(self):
-        if self.empty():
+        if self._tail is None:
             return None
-        val = self._shadow.pop(self._right, None)
-        last_idx = len(self._dll) - 1
-        self._dll.remove(index=last_idx)
-        self._right -= 1
+        n = self._tail
+        val = n.data
+        self._tail = n.prev
+        if self._tail is None:
+            self._head = None
+        else:
+            self._tail.next = None
+        self._size -= 1
         return val
 
     def front(self):
-        if self.empty():
-            return None
-        return self._shadow.get(self._left, None)
+        return None if self._head is None else self._head.data
 
     def back(self):
-        if self.empty():
-            return None
-        return self._shadow.get(self._right, None)
+        return None if self._tail is None else self._tail.data
 
     def empty(self):
-        return self._dll.is_empty()
+        return self._size == 0
 
     def count(self):
-        return 0 if self.empty() else (self._right - self._left + 1)
+        return self._size
 
     def clear(self):
-        while not self.empty():
-            self.pop_front()
-        self._shadow.clear()
-        self._left = 0
-        self._right = -1
+        i = self._head
+        while i is not None:
+            nxt = i.next
+            i.prev = None
+            i.next = None
+            i = nxt
+        self._head = None
+        self._tail = None
+        self._size = 0
 
-    def to_list(self):
+    def as_list(self):
         out = []
-        i = self._left
-        while i <= self._right:
-            out.append(self._shadow.get(i, None))
-            i += 1
+        i = self._head
+        while i is not None:
+            out.append(i.data)
+            i = i.next
         return out
 
-    def __len__(self):
-        return self.count()
+    def insert_front(self, data):
+        self.push_front(data)
+
+    def insert_rear(self, data):
+        self.push_back(data)
+
+    def remove_front(self):
+        if self._head is None:
+            raise IndexError("remove_front from empty queue")
+        return self.pop_front()
+
+    def remove_rear(self):
+        if self._tail is None:
+            raise IndexError("remove_rear from empty queue")
+        return self.pop_back()
+
+    def get_front(self):
+        if self._head is None:
+            raise IndexError("get_front from empty queue")
+        return self._head.data
+
+    def get_rear(self):
+        if self._tail is None:
+            raise IndexError("get_rear from empty queue")
+        return self._tail.data
+
+    def size(self):
+        return self._size
 
     def __iter__(self):
-        i = self._left
-        while i <= self._right:
-            yield self._shadow.get(i, None)
-            i += 1
+        i = self._head
+        while i is not None:
+            yield i.data
+            i = i.next
+
+    def __len__(self):
+        return self._size
 
     def __repr__(self):
-        return f"Deque(size={self.count()})"
+        return f"Deque(size={self._size})"
+
